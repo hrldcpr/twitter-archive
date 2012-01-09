0a3fcd1e68cadb8ab28256c3f4c8a078c184fee6
hrldcpr <hrldcpr@gmail.com>
2012-01-08 21:37:40 -0500

    max_id in search, despite twitter docs not mentioning it

--- twitter.py
+++ twitter.py
@@ -10,20 +10,27 @@ import urllib.error
 API_USER = 'http://api.twitter.com/1/statuses/user_timeline.json'
 API_SEARCH = 'http://search.twitter.com/search.json'
 
-DELAY = 24 # twitter rate-limit is 150 calls per hour i.e. 1 call per 24 seconds
+API_DELAY = 24 # API rate-limit is 150 calls per hour i.e. 1 call per 24 seconds
+SEARCH_DELAY = API_DELAY / 2 # search rate-limit is "less limited"
 
 def store_user_tweets(screen_name, include_rts=True, storage_path='tweets/'):
     storage_path += 'users/%s/' % screen_name
+    query = {'screen_name': screen_name, 'count': 200}
+    if include_rts:
+        query['include_rts'] = 't'
+    _store_tweets(API_USER, query, lambda x: x, storage_path, API_DELAY)
+
+def store_search_tweets(q, storage_path='tweets/'):
+    storage_path += 'searches/%s/' % urllib.parse.quote_plus(q)
+    query = {'q': q, 'rpp': 100, 'result_type': 'recent'}
+    _store_tweets(API_SEARCH, query, lambda x: x['results'], storage_path, SEARCH_DELAY)
+
+def _store_tweets(url, query, get_tweets, storage_path, delay):
     os.makedirs(storage_path, exist_ok=True)
     tweet_ids = (os.path.splitext(t) for t in os.listdir(storage_path))
     tweet_ids = {int(t[0]) for t in tweet_ids if t[1] == '.json'}
     # should we look for new tweets since last time?
     updating = True if tweet_ids else False
-
-    query = {'screen_name': screen_name,
-             'count': 200}
-    if include_rts:
-        query['include_rts'] = 't'
 
     while True:
         if updating:
@@ -31,12 +38,15 @@ def store_user_tweets(screen_name, include_rts=True, storage_path='tweets/'):
         elif tweet_ids:
             query['max_id'] = min(tweet_ids) - 1
 
-        tweets = _get_data(API_USER + '?' + urllib.parse.urlencode(query))
+        data = _get_data(url + '?' + urllib.parse.urlencode(query), delay)
+        tweets = get_tweets(data)
         _log('  got %d tweets' % len(tweets))
 
         if tweets:
-            _store_tweets(storage_path, tweets)
-            tweet_ids.update(t['id'] for t in tweets)
+            for t in tweets:
+                with open(storage_path + '%d.json' % t['id'], 'w') as f:
+                    json.dump(t, f)
+                tweet_ids.add(t['id'])
             _log('%d total tweets' % len(tweet_ids))
         else:
             if updating: # no more recent tweets, try old tweets
@@ -45,27 +55,10 @@ def store_user_tweets(screen_name, include_rts=True, storage_path='tweets/'):
             else: # no more old tweets, so we're done
                 break
 
-def store_search_tweets(q, storage_path='tweets/'):
-    storage_path += 'searches/%s/' % q
-    os.makedirs(storage_path, exist_ok=True)
-
-    query = {'q': q, 'result_type': 'recent', 'rpp': 100, 'page': 0}
-
-    while True:
-        query['page'] += 1
-        data = _get_data(API_SEARCH + '?' + urllib.parse.urlencode(query), delay=DELAY / 2)
-        tweets = data['results']
-        _log('  got %d tweets' % len(tweets))
-        if tweets:
-            _store_tweets(storage_path, tweets)
-        else:
-            break
-
-
 def _log(s):
     print(s, file=sys.stderr)
 
-def _get_data(url, delay=DELAY):
+def _get_data(url, delay):
     _log(url)
     while True:
         _log('  ...')
@@ -79,8 +72,3 @@ def _get_data(url, delay=DELAY):
         else:
             break
     return json.loads(data)
-
-def _store_tweets(storage_path, tweets):
-    for t in tweets:
-        with open(storage_path + '%d.json' % t['id'], 'w') as f:
-            json.dump(t, f)
